74a260d643c0b01290c7849aadf851244e61e72b
alex <89742348+sauc23@users.noreply.github.com>
2024-02-09 09:45:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const { exec } = require('child_process');
 const os = require('os');
 
-// Install @warren-bank/hls-proxy package globally
-exec('npm install --global "@warren-bank/hls-proxy"', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error installing @warren-bank/hls-proxy: ${error}`);
-    return;
-  }
   // Define the command to run HLS downloader with the specified user-agent and port
   const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
   const port = 3000;
